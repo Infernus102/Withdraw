72cf70b96d7baafd5115cd0859fb3d1f089ddaf4
Infernus <mr.snehjain@gmail.com>
2016-12-14 18:09:40 +0530

    Fix Economy

--- src/Muqsit/Withdraw.php
+++ src/Muqsit/Withdraw.php
@@ -93,7 +93,7 @@ class Withdraw extends PluginBase implements Listener{
 		if($this->money == null) return false;
 		switch($this->money->getName()){
 			case "EconomyAPI":
-				return $this->money->mymoney($p);
+				return $this->money->myMoney($p);
 			break;
 			case "EconomyPlus":
 				return $this->money->getInstance()->getMoney($p);
@@ -108,7 +108,7 @@ class Withdraw extends PluginBase implements Listener{
 		if($this->money == null) return false;
 		switch($this->money->getName()){
 			case "EconomyAPI":
-				$this->money->setMoney($p, $this->money->mymoney($p) + $money);
+				$this->money->addMoney($p, $money);
 			break;
 			case "EconomyPlus":
 				$this->money->getInstance()->setMoney($p, $this->money->getInstance()->getMoney($p) + $money);
